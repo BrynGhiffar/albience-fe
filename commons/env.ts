c6bac4f99557febe3cac174dc400c0e0769cb304
BrynGhiffar <bryn.ghiffar@gmail.com>
2023-01-12 09:13:25 +0700

    changed environment configuration to be localhost

--- commons/env.ts
+++ commons/env.ts
@@ -1,5 +1,5 @@
-// export const HOST = "http://localhost:3000";
-export const HOST = "https://fa52-202-80-217-88.ap.ngrok.io";
+export const HOST = "http://localhost:3000";
+// export const HOST = " https://8d0c-182-253-4-58.ap.ngrok.io";
 export const GET_CONFIGURATION = "/api/config";
 export const POST_CONFIGURATION = "/api/config";
 export const POST_PHOTO = "/api/photo";
